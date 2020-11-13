54e2329d6e03a69b4f794dd72989772c1df73e68
user <>
2020-11-13 08:08:36 +0100

    Add post controller

--- src/main/java/com/example/springswaggerpetstore/web/PetController.java
+++ src/main/java/com/example/springswaggerpetstore/web/PetController.java
@@ -1,17 +1,31 @@
 package com.example.springswaggerpetstore.web;
 
 import com.example.springswaggerpetstore.db.Pet;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Optional;
 
 @RestController
 public class PetController {
 
-    @GetMapping
-    Pet getPet(){
+    @GetMapping("/pet")
+    Pet getPet() {
         final Pet pet = new Pet();
         pet.setName("Peter");
         return pet;
+    }
+
+    @PostMapping
+    @RequestMapping("/pet/{petId}/uploadImage")
+    ResponseEntity<String> uploadImage(
+            @RequestParam("file") Optional<MultipartFile> file,
+            @PathVariable("petId") String petId) {
+        System.out.println("file = " + file);
+        System.out.println("petId = " + petId);
+        if (file.isPresent()) return ResponseEntity.ok("nice");
+        else return ResponseEntity.badRequest().body("pay the cat tax!");
     }
 
 }
